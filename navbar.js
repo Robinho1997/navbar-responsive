c470d19e7b38591baf6adcdbda184087fab7c217
Robinho1997 <robin.kempen@gmx.de>
2022-03-14 23:38:35 +0100

    fixed type erros

--- navbar.js
+++ navbar.js
@@ -1,14 +1,14 @@
-const mobileNavbar = document.querySelector("flex-navbar");
-const toggleNavbar = document.querySelector("button-close-or-open");
+const mobileNavbar = document.querySelector(".flex-navbar");
+const toggleNavbar = document.querySelector(".button-close-or-open");
 
 toggleNavbar.addEventListener("click", () => {
     const visibility = mobileNavbar.getAttribute("data-visible");
 
-      if ( visibility === false ) {
+        if ( visibility === "false" ) {
           mobileNavbar.setAttribute("data-visible", true);
           toggleNavbar.setAttribute("aria-expanded", true); 
 
-      }else if ( visibility === true ) {
+      } else if ( visibility === "true" ) {
           mobileNavbar.setAttribute("data-visible", false);
           toggleNavbar.setAttribute("aria-expanded", false);
       }

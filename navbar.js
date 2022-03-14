7520c73ba76648bf1886167edf2c759011b1cfef
Robinho1997 <robin.kempen@gmx.de>
2022-03-14 23:31:33 +0100

    button close/open logic

--- navbar.js
+++ navbar.js
@@ -1,2 +1,17 @@
 const mobileNavbar = document.querySelector("flex-navbar");
 const toggleNavbar = document.querySelector("button-close-or-open");
+
+toggleNavbar.addEventListener("click", () => {
+    const visibility = mobileNavbar.getAttribute("data-visible");
+
+      if ( visibility === false ) {
+          mobileNavbar.setAttribute("data-visible", true);
+          toggleNavbar.setAttribute("aria-expanded", true); 
+
+      }else if ( visibility === true ) {
+          mobileNavbar.setAttribute("data-visible", false);
+          toggleNavbar.setAttribute("aria-expanded", false);
+      }
+
+       
+})
